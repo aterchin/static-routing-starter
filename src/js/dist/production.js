31052f1ef3fcddfafd226ca666eeeb5a8215a3aa
Adam Terchin <adam.terchin@gmail.com>
2016-12-25 18:45:13 -0600

    adding latest jquery

---
+++ src/js/dist/production.js
@@ -0,0 +1,7 @@
+(function($){
+  $(document).ready(function(){
+    // do stuff
+  });
+}(jQuery));
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNjcmlwdHMuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6InByb2R1Y3Rpb24uanMiLCJzb3VyY2VzQ29udGVudCI6WyIoZnVuY3Rpb24oJCl7XG4gICQoZG9jdW1lbnQpLnJlYWR5KGZ1bmN0aW9uKCl7XG4gICAgLy8gZG8gc3R1ZmZcbiAgfSk7XG59KGpRdWVyeSkpO1xuIl19
